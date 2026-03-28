84b8abec5c68a0e9cacd1e22c438d0d1cbb6390b
Dimas Firmansyah <deirn@bai.lol>
2026-03-28 23:22:56 +0700

    init

---
+++ src/client/java/lol/bai/deirn/clientgametesttest/ClientgametesttestClient.java
@@ -0,0 +1,10 @@
+package lol.bai.deirn.clientgametesttest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientgametesttestClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
